d4494b162f26f8e9413f94c62dba5acd60a34c7a
Jay Lee <jay.lee031515@gmail.cm>
2021-09-10 11:15:24 -0700

    Fixed the path name for express.static

--- server/server.js
+++ server/server.js
@@ -2,21 +2,20 @@
 // Base API: https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo
 
 const express = require('express');
-// const path = require('path');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const productRouter = require('./products/server.js');
-const cartRouter = require('./cart/server.js');
-const q_aRouter = require('./questions_answers/server.js');
-const interactionRouter = require('./interactions/server.js');
-const reviewsRouter = require('./reviews/server.js');
+const productRouter = require('./products/server');
+const cartRouter = require('./cart/server');
+const q_aRouter = require('./questions_answers/server');
+const interactionRouter = require('./interactions/server');
+const reviewsRouter = require('./reviews/server');
 
-// app.use(express.static(path.join(__dirname,'/../react-client/dist')));
-
-// console.log(path.join(__dirname, 'client/public'));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use('/api/products', productRouter);
 app.use('/api/cart', cartRouter);
