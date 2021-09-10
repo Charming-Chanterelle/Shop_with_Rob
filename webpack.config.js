c4ffbd436e9214e82a92c60e4f1189086bfc3914
Bryanna <vbryanna94@gmail.com>
2021-09-09 17:45:38 -0700

    config edits after ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ module.exports = {
   entry: './client/src/index.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/client/public',
+    path: `${__dirname}/client/public`,
   },
 };
