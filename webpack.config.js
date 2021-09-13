4b094532b36cf7772d70eb110940caad2e8dedd2
sparangan <savanna.parangan@gmail.com>
2021-09-13 14:46:47 -0700

    Related items carousel shows one item at a time

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /(\.css)$/,
-        loaders: ['raw-loader', 'css-loader'],
-      },
+    //   {
+    //     test: /(\.css)$/,
+    //     loaders: ['raw-loader', 'css-loader'],
+    //   },
       {
         test: /\.(js|jsx)?/,
         exclude: /node_modules/,
