925f1e1dc32241068274db0b965853f76475957d
sparangan <savanna.parangan@gmail.com>
2021-09-14 16:48:19 -0700

    Add get requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { FaThumbsUp } from '@fortawesome/free-brands-svg-icons';
 import NavBar from './NavBar.jsx';
@@ -14,10 +16,49 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      product: {},
+      product_style: [],
+      meta_ratings: {},
     };
   }
 
+  componentDidMount() {
+    const randID = Math.ceil(Math.random() * (10 - 0));
+    console.log(randID);
+
+    axios.get('/api/products/?count=10')
+      .then((results) => {
+        this.setState({
+          product: results.data[randID],
+        })
+        axios.get(`/api/products/${this.state.product.id}/style`)
+          .then((results) => {
+            this.setState({
+              product_style: results.data.results,
+            })
+          })
+          .catch((err) => {
+            console.log('This is from Component Did Mount in the App Component', err);
+          })
+        axios.get(`/api/reviews/meta/?product_id=${this.state.product.id}`)
+          .then((results) => {
+            this.setState({
+              meta_ratings: results.data,
+            })
+          })
+          .catch((err) => {
+            console.log('This is from Component Did Mount in the App Component', err);
+          });
+      })
+      .catch((err) => {
+        console.log('This is from Component Did Mount in the App Component', err);
+      });
+  }
+
   render() {
+    const { product, product_style, meta_ratings } = this.state;
+    // console.log(product, product_style , meta_ratings);
+
     return (
       <div id="app">
         <NavBar />
