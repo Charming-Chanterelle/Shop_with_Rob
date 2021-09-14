e9df65731b491a51f9edc7daacbc6c005c88db1c
sparangan <savanna.parangan@gmail.com>
2021-09-13 17:35:44 -0700

    Edit CSS file

--- client/src/Outfit.jsx
+++ client/src/Outfit.jsx
@@ -120,38 +120,3 @@ const Outfit = (props) => {
 };
 
 export default Outfit;
-
-// import React from 'react';
-// import Carousel from 'react-elastic-carousel';
-
-// class Outfit extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       items: [
-//         { id: 1, title: 'item #1' },
-//         { id: 2, title: 'item #2' },
-//         { id: 3, title: 'item #3' },
-//         { id: 4, title: 'item #4' },
-//         { id: 5, title: 'item #5' },
-//       ],
-//     };
-//   }
-
-//   render() {
-//     const { items } = this.state;
-//     return (
-//       <>
-//         <h1 className="bigText">Your Outfit</h1>
-//         <Carousel itemsToShow={4}>
-//           {items.map((item) => (
-//             <>
-//               />
-//               <div key={item.id}>{item.title}</div>
-//             </>
-//           ))}
-//         </Carousel>
-//       </>
-//     );
-//   }
-// }
