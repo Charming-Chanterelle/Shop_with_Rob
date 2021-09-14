17487c723ebc934d3ea13d8fae3f2ba259189eb4
Jay Lee <jay.lee031515@gmail.cm>
2021-09-13 17:46:00 -0700

    First Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable import/extensions */
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FaThumbsUp } from '@fortawesome/free-brands-svg-icons';
 import NavBar from './NavBar.jsx';
 import Overview from './Overview.jsx';
 import Related from './Related.jsx';
 import Questions from './Questions.jsx';
-import Ratings from './RatingsList.jsx';
+import Ratings from './Ratings.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // value: 'hello worlddddd',
     };
   }
 
   render() {
-    // const { value } = this.state;
     return (
       <div id="app">
         Hello
