d7b0e52147cf326955573b47d6b42fe106cc0223
Bryanna <vbryanna94@gmail.com>
2021-09-14 21:00:02 -0700

    grouped last 2 axios get calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,10 @@ class App extends Component {
     super(props);
     this.state = {
       product: {},
-      product_styles: [], // all styles for this product (needed like this for overview if want overview to easily have the array to choose which style is rendered. Will need Overview to pass currentStyle to Related)
+      product_styles: [], // all styles for this product
+      // (needed like this for overview if want overview to
+      // easily have the array to choose which style is
+      // rendered. Will need Overview to pass currentStyle to Related)
       meta_ratings: {},
     };
   }
@@ -27,27 +30,21 @@ class App extends Component {
     axios.get('/api/products/?count=10')
       .then((products) => {
         const product = products.data[randID];
-        this.setState({ product });
-        return product.id;
+        return [product, product.id];
       })
-      .then((ID) => {
-        axios.get(`/api/products/${ID}/style`)
-          .then((styles) => {
-            this.setState({ product_styles: styles.data.results });
-          })
-          .catch((err) => {
-            console.log('error in client style GET', err);
-          });
-        axios.get(`/api/reviews/meta/?product_id=${ID}`)
-          .then((ratings) => {
-            this.setState({ meta_ratings: ratings.data });
-          })
-          .catch((err) => {
-            console.log('error in client ratings GET', err);
-          });
+      .then(([product, ID]) => {
+        axios.all([
+          axios.get(`/api/products/${ID}/style`),
+          axios.get(`/api/reviews/meta/?product_id=${ID}`),
+        ])
+          .then(axios.spread((data1, data2) => {
+            this.setState({
+              product, product_styles: data1.data.results, meta_ratings: data2.data,
+            });
+          }));
       })
       .catch((err) => {
-        console.log('error in client product GET', err);
+        console.log('error in client styles/ratings GET', err);
       });
   }
 
@@ -56,7 +53,7 @@ class App extends Component {
     return (
       <div id="app">
         <NavBar />
-        {/* <Overview product={product} styles={product_styles} stars={meta_ratings.ratings} /> */}
+        <Overview product={product} styles={product_styles} stars={meta_ratings.ratings} />
         <Related show={3} />
         <Outfit show={3} />
         <Questions />
