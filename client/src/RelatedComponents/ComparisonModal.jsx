eb3a71c70ef8ebe61fef00fceb3376ca4e8d5aa3
sparangan <savanna.parangan@gmail.com>
2021-09-15 13:54:45 -0700

    Add Comparison Modal skeleton

---
+++ client/src/RelatedComponents/ComparisonModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ComparisonModal = (props) => (
+  <div className="modal">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h4 className="modal-title">Comparing</h4>
+      </div>
+      <div className="modal-body">
+        This is modal content
+      </div>
+      <div className="modal-footer">
+        <button className="button" type="button">Close</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ComparisonModal;
