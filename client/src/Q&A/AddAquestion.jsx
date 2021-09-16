3f593f42106f34ab03ffa6f77ad1efcea7ee88b3
juan mejia <juanvmejia01@gmail.com>
2021-09-16 10:29:36 -0700

    updated button css and implented event handlers

--- client/src/Q&A/AddAquestion.jsx
+++ client/src/Q&A/AddAquestion.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const AddAquestion = () => (
-  <button type="button"> Add A Question </button>
-);
+const AddAquestion = () => {
+  const ButtonStyling = styled.button`
+  padding: 10px;
+  `;
+  return (
+    <ButtonStyling>
+      <button type="button" onClick={(e) => alert('In the future it will pop up a modal')}> Add A Question </button>
+    </ButtonStyling>
+  );
+};
 
 export default AddAquestion;
--- client/src/Q&A/Questions.jsx
+++ client/src/Q&A/Questions.jsx
@@ -9,35 +9,14 @@ font-weight: bolder;
 text-transform: uppercase;
 `;
 
-const Interactions = styled.div`
-  display: flex;
-  flex-direction: row;
-  font-size: small;
-  text-align: right;
-  position: relative;
-  left: 470px;
-  top: -7px;
-  text-transform: uppercase;
-`;
-
 const Questions = () => (
   <div>
+
     <QuestionsContainer>
       <span><h2>Q:</h2></span>
       <p>How do you center a div</p>
     </QuestionsContainer>
-    <Interactions>
-      <div>
-        Helpful?&nbsp;
-      </div>
-      <span>
-        YES (10)
-        <strong>&nbsp;</strong>
-      </span>
-      <span>
-        || No (2)
-      </span>
-    </Interactions>
+
   </div>
 );
 
