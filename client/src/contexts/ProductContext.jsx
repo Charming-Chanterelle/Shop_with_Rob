6888a160bddc60fe0cecbe3f4c3899ed6a4b38f5
Jay Lee <jay.lee031515@gmail.cm>
2021-09-15 19:13:57 -0700

    Updated the Product Context to add Product Styles and Meta Review

--- client/src/contexts/ProductContext.jsx
+++ client/src/contexts/ProductContext.jsx
@@ -4,37 +4,99 @@ import axios from 'axios';
 export const ProductContext = createContext();
 const randID = Math.ceil(Math.random() * (9 - 0));
 
+const getAverageRating = ({ ratings }) => {
+  // We want to get the total reviews and the average of the reviews.
+  const weight = Object.keys(ratings);
+  if (weight.length === 0) {
+    return {};
+  }
+  const rating = Object.values(ratings);
+
+  let numberOfRatings = 0;
+  let avgRating = 0;
+
+  for (let i = 0; i < weight.length; i++) {
+    const currentRating = Number(rating[i]);
+    const currentWeight = Number(weight[i]);
+
+    if (currentRating !== 0) {
+      numberOfRatings += currentRating;
+      avgRating += (currentRating * currentWeight);
+    }
+  }
+  avgRating /= numberOfRatings;
+
+  const ratingsObj = {
+    avgRating,
+    numberOfRatings,
+  };
+
+  return ratingsObj;
+};
+
 const ProductContextProvider = ({ children }) => {
   const [product, setProduct] = useState({});
   const [styles, setStyle] = useState([]);
+  const [meta, setMeta] = useState({});
+  const [ratingsScore, setRatingScore] = useState({});
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     console.log(window.location);
     axios.get('/api/products/?count=10')
       .then((products) => {
-        axios.get(`/api/products/${products.data[randID].id}/style`)
-          .then((style) => {
-            setProduct(products.data[randID]);
+        const currentProduct = products.data[randID];
+        return [currentProduct, currentProduct.id];
+      })
+      .then(([currentProduct, ID]) => {
+        axios.all([
+          axios.get(`/api/products/${ID}/style`),
+          axios.get(`/api/reviews/meta/?product_id=${ID}`),
+        ])
+          .then(axios.spread((style, metaReview) => {
+            console.log(currentProduct);
+            setProduct(currentProduct);
             setStyle(style.data.results);
-          })
-          .then(() => {
-            setLoaded(true);
-          })
+            setMeta(metaReview.data);
+            setRatingScore(getAverageRating(metaReview.data));
+          }))
           .catch((err) => {
-            console.log('Product Style Error:', err);
+            console.log('here is error', err);
           });
       })
+      .then(() => {
+        setLoaded(true);
+      })
       .catch((err) => {
-        console.log('Product Context Error:', err);
+        console.log('error in client styles/ratings GET', err);
       });
   }, []);
 
   return (
-    <ProductContext.Provider value={{ product, styles, loaded }}>
+    <ProductContext.Provider value={{
+      product, styles, meta, ratingsScore, loaded,
+    }}
+    >
       { children }
     </ProductContext.Provider>
   );
 };
 
 export default ProductContextProvider;
+
+// .then((products) => {
+//   axios.get(`/api/products/${products.data[randID].id}/style`)
+//     .then((style) => {
+//       setProduct(products.data[randID]);
+//       setStyle(style.data.results);
+//     })
+//     .then(() => {
+//       setLoaded(true);
+//     })
+//     .catch((err) => {
+//       console.log('Product Style Error:', err);
+//     });
+// })
+// .catch((err) => {
+//   console.log('Product Context Error:', err);
+// });
