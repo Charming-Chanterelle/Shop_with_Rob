1222c7012112f86788ef625994e4826a7d399eb5
sparangan <savanna.parangan@gmail.com>
2021-09-17 21:38:11 -0700

    Add raised effect to cards

--- client/src/RelatedComponents/RelatedStyles.jsx
+++ client/src/RelatedComponents/RelatedStyles.jsx
@@ -16,10 +16,17 @@ export const Card = styles.div`
   // z-index: 900;
   display: block;
   // flex-direction: column;
-  background-color: #D3D3D3;
+  background-color: #fffefa;
   border: 5px solid #899499;
 
-  padding: 15px;
+  padding: 10px;
+  border: 1px solid #d3d3d3;
+  // box-shadow:  -1px 1px #d3d3d3,
+  //      -2px 2px #77aaff,
+  //      -3px 3px #77aaff,
+  //      -4px 4px #77aaff,
+  //      -5px 5px #77aaff;
+  box-shadow: 2px 2px 2px 1px #d3d3d3;
   margin: 10px 50px;
 
   `;
