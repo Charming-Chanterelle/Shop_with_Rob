585e48a8d8af9f1afb109fd34b0e8e959c9a067d
juan mejia <juanvmejia01@gmail.com>
2021-09-21 03:13:32 -0700

    finished almost all functionality using hardData

--- client/src/Q&A/App.jsx
+++ client/src/Q&A/App.jsx
@@ -1,47 +1,111 @@
-/* eslint-disable import/extensions */
-import React, { useState } from 'react';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
-import QandAitems from './QandAitems.jsx';
-import SearchBar from './SearchBar.jsx';
-import LoadMore from './LoadMore.jsx';
-import AddAquestion from './AddAquestion.jsx';
+import Search from './Search.jsx';
+import QandAContainer from './QandAContainer.jsx';
+import MoreQuestions from './MoreQuestions.jsx';
+import QuestionModal from './QuestionModal.jsx';
 
 const Container = styled.div`
   width: 700px;
   margin: auto;
-  padding-top: 100px;
-  padding-bottom: 100px;
 `;
 
-const ButtonSpaceing = styled.button`
-margin-left: 10px;
+const Button = styled.button`
+  border: 2px solid white;
+  padding: 1em;
+  width: 200px;
+  margin: 0px 10px;
+  cursor: pointer;
+  background-color: #C7CFB7;
+  color: #525252;
 `;
 
-const App = () => {
-  const [value] = useState('HAVE A QUESTION? SEARCH FOR ANSWERS... ');
-  const search = useState('');
-  const yesCounter = useState(0);
-  const noCounter = useState(0);
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px;
+`;
 
-  const searchValue = (event) => {
-    event.preventDefault();
-    search(event.target.value);
+const App = ({ productId = 48432 }) => {
+  const [questions, setQuestions] = useState([]);
+  const [questionShow, setQuestionShow] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [productName, setProductName] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [counter, setCounter] = useState(2);
+  const [noQuestions, setNoQuestions] = useState(false);
+
+  const getQuestions = () => (
+    axios.get('/api/qa/questions', {
+      params: {
+        product_id: productId,
+      },
+    })
+      .then((response) => {
+        setQuestions(response.data.data.results);
+      })
+      .catch((error) => (
+        error
+      ))
+  );
+
+  const getProductInfo = () => {
+    axios.get(`/api/products/${productId}`, {
+      params: {
+        product_id: productId,
+      },
+    })
+      .then((response) => {
+        setProductName(response.data.data.name);
+      });
   };
-  const submited = (event) => {
-    event.preventDefault();
-    const val = event.target.value;
-    alert(`this is what was submited ${val}`);
-    search('');
+
+  useEffect(getQuestions, []);
+  useEffect(getProductInfo, []);
+
+  const incrementQuestionCount = () => {
+    let newCounter;
+    if (counter < (questions.length - 2)) {
+      newCounter = counter + 2;
+    } else {
+      newCounter = questions.length;
+      setNoQuestions(true);
+    }
+    setCounter(newCounter);
   };
+
+  const searchQuestions = () => {
+    setSearchResults([]);
+    const results = new Set();
+    questions.forEach((question) => {
+      if (question.question_body.toLowerCase().includes(searchTerm.toLowerCase())) {
+        results.add(question);
+      }
+    });
+    setSearchResults(Array.from(results));
+  };
+
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+    if (event.target.value.length >= 3) {
+      searchQuestions();
+    } else if (event.target.value.length < 3) {
+      setSearchResults([]);
+    }
+  };
+
   return (
     <Container>
-      <h1 className="bigText">Questions and Answers</h1>
-      <SearchBar value={value} onChange={searchValue} submited={submited} />
-      <QandAitems yesCounter={yesCounter} noCounter={noCounter} />
-      <LoadMore />
-      <ButtonSpaceing>
-        <AddAquestion />
-      </ButtonSpaceing>
+      <h1>Questions and Answers</h1>
+      <Search searchTerm={handleSearchTerm} searchQuestions={searchQuestions} questions={questions} />
+      <QandAContainer questions={searchResults.length ? searchResults : questions} productName={productName} counter={counter} />
+      <Buttons>
+        <MoreQuestions noQuestions={noQuestions} incrementQuestionCount={incrementQuestionCount} />
+        <Button type="submit" onClick={() => setQuestionShow(true)}>ADD A QUESTION + </Button>
+      </Buttons>
+      <QuestionModal onClose={() => setQuestionShow(false)} show={questionShow} productName={productName} productId={productId} />
     </Container>
   );
 };
