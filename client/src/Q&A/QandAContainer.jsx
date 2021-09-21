2d55a5a2c8db860a4de201b18cdabb3c29225223
juan mejia <juanvmejia01@gmail.com>
2021-09-21 03:13:32 -0700

    finished almost all functionality using hardData

---
+++ client/src/Q&A/QandAContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import QandAItem from './QandAItem.jsx';
+
+const QandAContainer = ({ questions, productName, counter }) => {
+  const sortedQuestions = questions;
+  sortedQuestions.sort((a, b) => (a.helpfulness > b.helpfulness));
+
+  const limitQuestions = sortedQuestions.slice(0, counter);
+
+  return (
+    <div>
+      {limitQuestions.map((question) => (
+        <QandAItem questions={question} productName={productName} />
+      ))}
+    </div>
+  );
+};
+
+export default QandAContainer;
---
+++ client/src/Q&A/Search.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border: 1px solid #525252;
+  padding: 1em;
+  width: 700px;
+  color: #525252;
+`;
+
+const Search = ({ searchTerm, questions }) => {
+  const { length } = questions;
+
+  return (
+    <div>
+      {length ? (
+        <form>
+          <Input placeholder="Have a question? Search for answers…" onChange={searchTerm} />
+        </form>
+      ) : null}
+    </div>
+  );
+};
+
+export default Search;
