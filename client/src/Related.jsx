05aeb45bf5d8f04584be7772b61bb9c3b1aeb9a2
sparangan <savanna.parangan@gmail.com>
2021-09-22 07:19:03 -0700

    Get ratings from API

--- client/src/Related.jsx
+++ client/src/Related.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { FaRegStar } from 'react-icons/fa';
 import axios from 'axios';
@@ -21,12 +22,55 @@ const Related = (props) => {
     setLength(items.length);
   }, [items]);
 
-  const getRelatedProductIds = () => {
+  const getAverageRating = (ratings) => {
+    // We want to get the total reviews and the average of the reviews.
+    const weight = Object.keys(ratings);
+    if (weight.length === 0) {
+      return {
+        avgRating: 0,
+        numberOfRatings: 0,
+      };
+    }
+    const rating = Object.values(ratings);
+
+    let numberOfRatings = 0;
+    let avgRating = 0;
+    const ratingsPercent = {};
+
+    for (let i = 0; i < weight.length; i++) {
+      const currentRating = Number(rating[i]);
+      const currentWeight = Number(weight[i]);
+
+      if (currentRating !== 0) {
+        numberOfRatings += currentRating;
+        avgRating += (currentRating * currentWeight);
+      }
+    }
+
+    for (let j = 0; j < weight.length; j++) {
+      const ratingPercent = Number(rating[j]);
+      const weightPercent = weight[j];
+      if (ratingPercent !== 0) {
+        ratingsPercent[weightPercent] = parseFloat(ratingPercent / numberOfRatings).toFixed(2);
+      }
+    }
+    avgRating /= numberOfRatings;
+
+    const ratingsObj = {
+      avgRating,
+      numberOfRatings,
+      ratingsPercent,
+    };
+
+    return ratingsObj;
+  };
+
+  const getRelatedProducts = () => {
     axios.get(`/api/products/${overviewID}/related`)
       .then((response) => {
         const productIDs = response.data;
         return productIDs.map((id) => (
-          [axios.get(`/api/products/${id}`), axios.get(`/api/products/${id}/style`)]
+          [axios.get(`/api/products/${id}`), axios.get(`/api/products/${id}/style`), axios.get(`api/reviews/meta/?product_id=${id}`)]
 
         ));
       })
@@ -40,13 +84,19 @@ const Related = (props) => {
         // have array of arrays
         // need data from nested array
         // eslint-disable-next-line implicit-arrow-linebreak
-        resolved.map((array) => [array[0].data, array[1].data.results]))
-      .then((fourth) => fourth.map((array) => ({
-        product: array[0],
-        styles: array[1],
-      })))
+        resolved.map((array) => [array[0].data, array[1].data.results, array[2].data.ratings]))
+      .then((fourth) =>
+        // console.log('fourth: ', fourth);
+        fourth.map((array) =>
+          // let rating = array[2];
+          // const avg = ((1*))
+          ({
+            product: array[0],
+            styles: array[1],
+            reviews: getAverageRating(array[2]).avgRating,
+          })))
       .then((fifth) => {
-        console.log(fifth);
+        // console.log(fifth);
         setRelatedItems(fifth);
       })
 
@@ -61,7 +111,7 @@ const Related = (props) => {
       });
   };
 
-  useEffect(getRelatedProductIds, []);
+  useEffect(getRelatedProducts, []);
   const next = () => {
     if (currentIndex < (length - show)) {
       setCurrentIndex((prevState) => prevState + 1);
@@ -95,6 +145,7 @@ const Related = (props) => {
               }
           <s.CardWrapper
             className="carousel-content-wrapper"
+            data-testid="carousel-content-wrapper"
           >
             <div
               className={`carousel-content show-${show}`}
@@ -130,7 +181,7 @@ const Related = (props) => {
                       </div>
                       <div className="bigText">
                         Rating:
-                        {` ${item.product.rating}`}
+                        {` ${item.reviews.toPrecision(3)}`}
                       </div>
                     </s.CardText>
                   </div>
@@ -156,3 +207,13 @@ const Related = (props) => {
 };
 
 export default Related;
+
+// // eslint-disable-next-line max-len
+// console.log('object: ', obj);
+// const sum = 1 * obj.reviews[1] + 2 * obj.reviews[2] + 3 * obj.reviews[3] + 4 * obj.reviews[4] + 5 * obj.reviews[5];
+// // eslint-disable-next-line max-len
+// console.log('sum: ', sum);
+// // eslint-disable-next-line max-len
+// const avg = sum / (obj.reviews[1] + obj.reviews[2] + obj.reviews[3] + obj.reviews[4] + obj.reviews[5]);
+// console.log('avg: ', avg);
+// obj.reviews = avg;
--- client/src/tests/Related.test.jsx
+++ client/src/tests/Related.test.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import React from 'react';
-
 import { render, screen, cleanup } from '@testing-library/react';
+import axios from 'axios';
+import Related, { getRelatedProducts } from "../Related.jsx"
 // import 'jest-dom/extend-expect';
-import Related from '../Related';
+import 'regenerator-runtime/runtime';
 
 afterEach(cleanup);
 
@@ -11,8 +13,14 @@ describe('Related Items', () => {
     const { getByText } = render(<Related />);
     expect(getByText('Related Items')).toBeDefined();
   });
-  // it('should include an individual container for each carousel element', () => {
-  //   const { getByTestId } = render(<Related />);
-  //   expect(getByTestId('carousel-content-wrapper')).toBeDefined();
-  // });
+  it('should include an individual container for each carousel element', () => {
+    const { getByTestId } = render(<Related />);
+    expect(getByTestId('carousel-content-wrapper')).toBeDefined();
+  });
+  it('should retrieve related items, async', async () => {
+    axios.get.mockResolvedValueOnce(data);
+    const relatedItems = await getRelatedProducts();
+    expect(axios.get).toHaveBeenCalledWith('api/products/48434/related');
+    expect(relatedItems).toEqual(data);
+  });
 });
