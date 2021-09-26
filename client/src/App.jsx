99953e83de0acd3bf90f517f8270d18214898a2c
sparangan <savanna.parangan@gmail.com>
2021-09-25 22:34:24 -0700

    Overview and related successfully merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import ProductContextProvider from './contexts/ProductContext.jsx';
 // import { FaThumbsUp } from '@fortawesome/free-brands-svg-icons';
 import NavBar from './NavBar.jsx';
 import Overview from './Overview.jsx';
-const Related = React.lazy(() => import ('./Related.jsx'))
-const Questions = React.lazy(() => import ('./Q&A/App.jsx'))
+import Related from './Related.jsx';
+import Outfit from './Outfit.jsx';
+const Questions = React.lazy(() => import ('./Q&A/App.jsx'));
 const Ratings = React.lazy(() => import ('./Ratings.jsx'));
-const Outfit = React.lazy(() => import ('./Outfit.jsx'));
+
 
 function App() {
   const scrollToDiv = (ref) => window.scrollTo(0, ref.current.offsetTop);
@@ -21,8 +22,6 @@ function App() {
       <Related show={4} />
       <Outfit show={4} />
       <Suspense fallback={<div>Loading...</div>}>
-        <Related show={4} />
-        <Outfit show={4} />
         <Questions />
         <Ratings reference={el2}/>
       </Suspense>
