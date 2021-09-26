244320aa5da9e07e41d2d313b5e07d60c9ab85be
Jay Lee <jay.lee031515@gmail.cm>
2021-09-26 03:18:03 -0700

    Updated Product id in the init redis file

--- client/initRedisData.js
+++ client/initRedisData.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const productID = 48432;
+const productID = 48445;
 
 const InitData = (ID, count) => {
   const product = axios.get(`http://localhost:3000/api/products/${ID}`);
